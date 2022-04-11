03fb7f5516bd3cbd580a5c392d18c41c64407147
deyanazhari <deyanazhari@gmail.com>
2022-04-11 16:35:32 +0700

    Update search

--- components/Products.vue
+++ components/Products.vue
@@ -1,34 +1,73 @@
 <template>
   <v-row>
-    <v-col v-for="(product, index) in products" cols="2">
-      <v-card :title="product.title">
-        <v-card-actions>
-          <v-img
-            :src="require(`@/assets/images/products/$(product.thumbnail)`)"
-          >
-          </v-img>
-        </v-card-actions>
-        <v-card-text>
-          {{ product.title }}
-        </v-card-text>
-      </v-card>
-    </v-col>
+    <v-row>
+      <v-col cols="10">
+        <v-autocomplete
+          label="Products"
+          placeholder="Start typing to search"
+          :search-input.sync="search"
+          :loading="isLoading"
+        >
+        </v-autocomplete>
+      </v-col>
+      <v-col cols="2">
+        <v-menu>
+          <template v-slot:activator="{ on: category }">
+            <v-btn v-on="category" color="primary"> Category</v-btn>
+          </template>
+          <v-list>
+            <v-list-item-group v-model="categoryId">
+              <v-list-item
+                v-for="(category, index) in categories"
+                :key="index"
+                :value="category.id"
+                :disabled="categoryId == categoryId"
+              >
+                <v-list-item-title>
+                  {{ category.title }}
+                </v-list-item-title>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-menu>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col v-for="(product, index) in filteredProducts" :key="index" cols="2">
+        <v-card :title="product.title" :ripple="true">
+          <v-card-actions>
+            <v-img
+              :src="require(`@/assets/images/products/${product.thumbnail}`)"
+            >
+            </v-img>
+          </v-card-actions>
+          <v-card-text align="center" class="product-title">
+            {{ product.title }}
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-row>
 </template>
 
 <script>
-import { defineComponent } from '@vue/composition-api'
-
 export default {
   data() {
     return {
+      categoryId: false,
+      categories: [
+        { id: false, title: 'All' },
+        { id: 1, title: 'Smartphone' },
+        { id: 2, title: 'Kamera' },
+        { id: 3, title: 'Televisi' },
+      ],
       products: [
         {
           id: 1,
-          title: 'asus Zenfone',
+          title: 'Asus Zenfone',
           thumbnail: 'asus-zenfone.png',
           price: 2000000,
-          category: 1,
+          categoryId: 1,
         },
         {
           id: 2,
@@ -129,7 +168,34 @@ export default {
           categoryId: 3,
         },
       ],
+      search: null,
+      isLoading: false,
     }
+  },
+  computed: {
+    filteredProducts() {
+      if (this.categoryId) {
+        return this.products.filter((s) => s.categoryId == this.categoryId)
+      }
+      return this.products
+    },
+  },
+  watch: {
+    search(val) {
+      console.log(val)
+      this.isLoading = true
+      setTimeout(() => {
+        this.isLoading = false
+      }, 1000)
+    },
   },
 }
 </script>
+<style>
+html,
+.product-title {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <v-row>
-    <v-col cols="8"> PRODUCTS </v-col>
-    <v-col cols="4"> order </v-col>
+    <v-col cols="8" class="frame-content px-6"> <products></products> </v-col>
+    <v-col cols="4" class="frame-content white"> Order</v-col>
   </v-row>
 </template>
+
